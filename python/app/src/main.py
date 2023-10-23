a6aa7c37a775224b8935a4786f75a53c35e13f99
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2023-10-23 15:22:49 +1100

    Created confirmation

--- python/app/src/main.py
+++ python/app/src/main.py
@@ -1,5 +1,6 @@
 import sys
 
+# Calclaration
 class Calculation:
     def __init__(self, balance):
         self.balance = balance
@@ -8,103 +9,81 @@ class Calculation:
         self.balance += amount
 
     def get_balance(self):
-        return print(self.balance)
+        return print(f"Your total prine is ${self.balance}")
+    
+    # def get_plan(self, name):
+    #     return print(self.name)
     
     def reset_balance(self):
         self.balance = 0
 
+# class Display:
+#     def __init__(self, plan , main_dish, dessert, alcohol):
+#         self.plan = plan
+#         # self.main_dish = main_dish
+#         # self.dessert = dessert
+#         # self.alcohol = alcohol
+#     def set_plan(self, plan):
+#         self.plan = plan
+#     def get_plan(self):
+#         return print(self.plan)
+
+plans = []
+
 # Set user balance
-user = Calculation(balance=0)
 # balance = 0
 # user.deposit(100)
 # user.deposit(100)
 # user.get_balance()
 
-
-# display a main
-# display shows up when the user clicks
-# Need to make it easy to see
-
-
-
-def welcome():
-    print("")
-    print("*** Welcome to the catering services for your party! ***")
-    print("")
-    print("Would you like to have a look?")
-    print("")
-    user_select = input("Please press y/n: ")
-    if user_select == "y":
-        description()
-    elif user_select == "n":
-        print("Thank you very much")
-        sys.exit(0)
-    else:
-        print("Something went wrong")
-
-    budget()
-# Amount function
-
-
-
-
-
-
-
-# description
+user = Calculation(balance=0)
+# user = Display(plan=None)
 # More information for user to understand our service easily
-def description():
-    pass
+
 # Budget checking
 def budget():
-    print("Please choose your budget for your party")
-    print("")
-    print("")
-    print("$ 100 Chinese food, Press '1'")
-    print("")
-    print("$ 150 Mexican food, Press '2'")
-    print("")
-    print("$ 200  Thai  food,  Press '3'")
-    print("")
-    plan()
+    print(f"\n"
+    "*** Welcome to the catering services for your party! ***"
+    "\n")
 
+    print("Please choose your budget for your party"
+    "\n"
+    "\n"
+    "$ 100 Chinese food, Press '1'\n"
+    "\n"
+    "$ 150 Mexican food, Press '2'\n"
+    "\n"
+    "$ 200  Thai  food,  Press '3'\n"
+    "\n"
+    )
+# Display what user chose at the end
 # choose a plan
 # Need to make error handling
 def plan():
     # while True:
         user_plan = input("Please press the number: ")
-        total_price = 0
         if user_plan == '1':
-            # total_price(100)
-            # print(total_price)
+            plans.append("Chinese food")
+            # print(plans)
             user.deposit(100)
-            user.get_balance()
+            # user = Calculation(name="Cninese")
             chinese_food()
             # $100
         elif user_plan == '2':
-            # total_price(150)
-            # print(total_price)
+            plans.append("Mexican food")
             user.deposit(150)
-            user.get_balance()
             mexicon_food()
             # $150
         elif user_plan == '3':
-            # total_price(200)
-            # print(total_price)
+            plans.append("Thai food")
             user.deposit(200)
-            user.get_balance()
             thai_food()
             # $200
         else:
             print("Something went wrong. Please try again")
-            # try_again = input("Please press y/n: ")
-            # if try_again == 'y':
-            # break
-        # print("Something went wrong. Please try again")
-            # return print("Something went wrong. Please try again")
 
 
-
+# Display the main what the user chose at the end
 # Chinese food function
 # Need to make error handling
 # Need to create '2' and '3'
@@ -129,17 +108,18 @@ def chinese_food():
 
         if user_dish == '1':
             user.get_balance()
-            option1()
+            plans.append("Chicken Fried Rice")
+            dessert1()
             # $100
         elif user_dish == '2':
             user.deposit(20)
-            user.get_balance()
-            option1()
+            plans.append("Char Siu Park")
+            dessert1()
             # #120
         elif user_dish == '3':
             user.deposit(40)
-            user.get_balance()
-            option1()
+            plans.append("Mongolian Beef")
+            dessert1()
             # $140
         else:
             print("Something went wrong. Please try again")
@@ -163,17 +143,18 @@ def mexicon_food():
 
         if user_dish == '1':
             user.get_balance()
-            option2()
+            plans.append("Mexican Lime Chicken")
+            dessert2()
             # $150
         elif user_dish == '2':
             user.deposit(20)
-            user.get_balance()
-            option2()
+            plans.append("Pork Carnites")
+            dessert2()
             # $170
         elif user_dish == '3':
             user.deposit(40)
-            user.get_balance()
-            option2()
+            plans.append("Barbacoa Beef")
+            dessert2()
             # $190
         else:
             print("Something went wrong. Please try again")
@@ -198,17 +179,18 @@ def thai_food():
 
         if user_dish == '1':
             user.get_balance()
-            option3()
+            plans.append("Thai Fried Cheiken")
+            dessert3()
             # $200
         elif user_dish == '2':
             user.deposit(20)
-            user.get_balance()
-            option3()
+            plans.append("Thai Basil Pork Belly")
+            dessert3()
             # $220
         elif user_dish == '3':
             user.deposit(40)
-            user.get_balance()
-            option3()
+            plans.append("Pad Gra Prow")
+            dessert3()
             # $240
         else:
             print("Something went wrong. Please try again")
@@ -216,53 +198,19 @@ def thai_food():
 def dish():
 
         pass
-# choose option
-def option1():
-    # while True:
-        print("")
-        print("Would you like to add DESSERTS?")
-        user_opt = input("Please press y/n: ")
-        if user_opt.lower() == 'y':
-            dessert1()
-        elif user_opt.lower() == 'n':
-            a_option()
-        else:
-            print("Something went wrong. Please try again")
 
-def option2():
-    # while True:
-        print("")
-        print("Would you like to add DESSERTS?")
-        user_opt = input("Please press y/n: ")
-        if user_opt.lower() == 'y':
-            dessert2()
-        elif user_opt.lower() == 'n':
-            a_option()
-        else:
-            print("Something went wrong. Please try again")
-
-def option3():
-    # while True:
-        print("")
-        print("Would you like to add DESSERTS?")
-        user_opt = input("Please press y/n: ")
-        if user_opt.lower() == 'y':
-            dessert3()
-        elif user_opt.lower() == 'n':
-            a_option()
-        else:
-            print("Something went wrong. Please try again")
-
+# Display the deseert what the user entered
 # desserts
 def dessert1():
     # while True:
         print("")
+        print("Would you like to add DESSERTS?")
         print("[+$10] Tofu Pudding")
         # chinese dessert inputs
         chi_des = input("Please press y/n: ")
         if chi_des.lower() == 'y':
             user.deposit(10)
-            user.get_balance()
+            plans.append("Tofu Pudding")
             a_option()
         elif chi_des.lower() == 'n':
             a_option()
@@ -273,12 +221,13 @@ def dessert1():
 def dessert2():
     # while True:
         print("")
+        print("Would you like to add DESSERTS?")
         print("[+$10] Sweet Mexican corn cake")
 
         chi_des = input("Please press y/n: ")
         if chi_des.lower() == 'y':
             user.deposit(10)
-            user.get_balance()
+            plans.append("Sweet Mexican corn cake")
             a_option()
         elif chi_des.lower() == 'n':
             a_option()
@@ -289,12 +238,13 @@ def dessert2():
 def dessert3():
     # while True:
         print("")
+        print("Would you like to add DESSERTS?")
         print("[+$10] Banana Roti")
 
         chi_des = input("Please press y/n: ")
         if chi_des.lower() == 'y':
             user.deposit(10)
-            user.get_balance()
+            plans.append("Banana Roti")
             a_option()
         elif chi_des.lower() == 'n':
             a_option()
@@ -312,6 +262,7 @@ def a_option():
             allergy()
         else:
             print("Something went wrong. Please try again")
+# Display alcohol information        
 # alcoholic
 
 def alcohol():
@@ -323,7 +274,7 @@ def alcohol():
         chi_des = input("Please press y/n: ")
         if chi_des.lower() == 'y':
             user.deposit(30)
-            user.get_balance()
+            plans.append("BEER, WINE, SAKE")
             allergy()
         elif chi_des.lower() == 'n':
             allergy()
@@ -332,7 +283,7 @@ def alcohol():
     # fill allergy
     # Input form
 
-
+# Display the user's allergy  
 def  allergy():
     
     print("")
@@ -340,18 +291,20 @@ def  allergy():
     user_allergy = input("Do you have any food allergies?: ")
     print("")
     print(f"Your food allergy: {user_allergy}")
-    conform()
+    confirm()
 
 
 
-# conform display
+# Confirm what is your plan finally
+# plan, main dish, desserts, alcohol and allergy
 # Need to be with plan and total
-def conform():
+def confirm():
     print("")
     print("Please comfirm your plan")
+
     # print(total_price)
     user.get_balance()
-    print("")
+    print(plans)
     end()
 
 # try again or exit
@@ -360,17 +313,22 @@ def end():
         user_exit = input("Please press y/n: ")
         if user_exit == 'y':
             user.reset_balance()
-            welcome()
+            plans.clear()
+            main()
+
         elif user_exit == 'n':
             print("")
             print("Thank you for choosing us.")
             sys.exit(0)
             
-            
-            
         else:
             print("Thank you for choosing us.")
             sys.exit(1)
-            
-            
-welcome()
+    
+# main
+def main():
+
+    budget()
+    plan()
+
+main()
